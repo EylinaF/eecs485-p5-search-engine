9d735b88f1e3c739b5f3e38d75d039fd51d4b043
eylina <eylinaf@umich.edu>
2025-04-10 17:15:11 -0400

    finish job0

--- inverted_index/map0.py
+++ inverted_index/map0.py
@@ -1,1 +1,7 @@
 """Map 0."""
+import sys
+
+
+for line in sys.stdin:
+    if "<!DOCTYPE html>" in line:
+        print("{count}\t1")
--- inverted_index/reduce0.py
+++ inverted_index/reduce0.py
@@ -1,1 +1,27 @@
 """Reduce 0."""
+import sys
+import itertools
+
+
+def main():
+    """Divide sorted lines into groups that share a key."""
+    for _, group in itertools.groupby(sys.stdin, keyfunc):
+        reduce_one_group(group)
+
+
+def keyfunc(line):
+    """Return the key from a TAB-delimited key-value pair."""
+    return line.partition("\t")[0]
+
+
+def reduce_one_group(group):
+    """Reduce one group."""
+    total_count = 0
+    for line in group:
+        count = line.partition("\t")[2]
+        total_count += int(count)
+    print(f"{total_count}")
+
+
+if __name__ == "__main__":
+    main()
